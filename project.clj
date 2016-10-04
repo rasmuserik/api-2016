ccb8a5f01637a6cd44aa106c29aa882f72150ec7
Rasmus Erik Voel Jensen <github-rasmuserik@solsort.dk>
2016-10-04 08:21:13 +0200

    dependencies etc.

--- project.clj
+++ project.clj
@@ -56,7 +56,7 @@
      {:output-to "index.js"
       :main solsort.api.api
       :externs ["util/externs.js"]
-      :optimizations :advanced
+      :optimizations :simple
       :pretty-print false}}
     ]}
 )
